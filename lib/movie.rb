f65adca2f6edf1436639a40a9018b07626028c63
MaxPepega <budkomaksim16@gmail.com>
2023-07-31 22:34:18 +0500

    add update to book and movie

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,6 +1,6 @@
 class Movie < Product
 
-  attr_reader :title, :year, :director
+  attr_accessor :title, :year, :director
 
   def initialize(params)
     super
@@ -14,4 +14,11 @@ class Movie < Product
     "Фильм <<#{@title}>>, #{@year}, реж. - #{@director}, #{super}"
   end
 
+  def update(params)
+    super
+
+    @title = params[:title] if params[:title]
+    @year = params[:year] if params[:year]
+    @director = params[:director] if params[:director]
+  end
 end

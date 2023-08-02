3da80f4f4e605c1d1ce8410e6ab0de4829aa85bf
MaxPepega <budkomaksim16@gmail.com>
2023-08-02 22:30:59 +0500

    add product collection

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/movie'
+require_relative 'lib/product_collection'
 
 product = []
 
@@ -33,3 +34,10 @@ puts leon
 
 films_from_file = Book.from_file('./data/books/01.txt')
 puts films_from_file
+puts
+puts "_____________________________________________________________"
+collection = ProductCollection.from_dir(File.dirname(__FILE__ ) + '/data')
+collection.sort!(by: :price , order: :asc)
+collection.to_a.each do |product|
+  puts product
+end
